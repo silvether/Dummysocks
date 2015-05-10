945525026f20517b7ad34aa7697bc22a5d6bf248
huoxy <huoxy@wandoujia.com>
2015-05-10 22:22:42 +0800

    Initial Commit

---
+++ Dummysocks/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Dummysocks
+//
+//  Created by huoxy on 15/5/10.
+//  Copyright (c) 2015年 huoxy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
